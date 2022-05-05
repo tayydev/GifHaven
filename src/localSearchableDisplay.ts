8d48dec2a5dee5b98fe720aba25f5826cb12f96f
NathanNorth <51377975+NathanNorth@users.noreply.github.com>
2022-05-05 11:06:44 -0400

    convert a lot more code to typescript

--- src/localSearchableDisplay.ts
+++ src/localSearchableDisplay.ts
@@ -26,31 +26,6 @@ export class LocalSearchableDisplay extends Display {
         super.draw(gifs)
     }
 
-    // public draw(filter: RegExp = matchAll) {
-    //     //clear
-    //     const library = document.getElementById('display')
-    //     while(library.firstChild) {
-    //         library.removeChild(library.firstChild)
-    //     }
-    //
-    //     //draw
-    //     const gifs = this.io.getLibrary().gifs
-    //         .sort((a, b) => b.timestamp - a.timestamp) //reverse chronological
-    //         .filter(gif => filter.test(gif.name))
-    //
-    //     console.log(gifs.length)
-    //     console.log(filter.source)
-    //
-    //     for(const gif of gifs) {
-    //         console.log("Found gif: " + gif)
-    //         const img = document.createElement('img')
-    //         img.classList.add('gif')
-    //         img.src = gif.path
-    //         img.setAttribute('data-path', gif.path)
-    //         library.appendChild(img)
-    //     }
-    // }
-
     public add(gif: Gif): void {
         const lib = this.io.getLibrary()
         lib.gifs.unshift(gif) //addFirst equivalent
